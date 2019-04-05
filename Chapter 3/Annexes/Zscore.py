a2baab5e3ff90cd07ab4ea7de71dae6d997e0653
Chikoungoun <marwane.lotfi@gmail.com>
2019-04-05 12:57:17 +0100

    Z-score computing function

---
+++ Chapter 3/Annexes/Zscore.py
@@ -0,0 +1,9 @@
+""" Z score Function
+
+x     : observation from a distribution
+mu    : mean of the distribution
+sigma : Standard deviation of the distribution
+"""
+
+def zscore(x,mu,sigma):
+    return (x-mu)/sigma
